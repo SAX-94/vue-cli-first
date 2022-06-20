5488f6b785424d1495551921dc3af0c3e8519e25
SAX_94 <matteosacchi94@gmail.com>
2022-06-20 23:30:39 +0200

    created JumboTron component

---
+++ src/components/JumboTron.vue
@@ -0,0 +1,12 @@
+<template>
+    <div>
+        <img src="https://i.ytimg.com/vi/SA_bjDg769M/maxresdefault.jpg" alt="jumbotron" srcset="">    
+    </div>
+</template>
+
+<script>
+
+export default  {
+
+}
+</script>
